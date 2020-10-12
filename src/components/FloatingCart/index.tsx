a65cca9fb0b93723a542f83485ab92e9245b15cf
Evellyn Costa <ecosta.inf@gmail.com>
2020-10-12 13:03:04 -0300

    challenged concluded

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -18,6 +18,14 @@ import { useCart } from '../../hooks/cart';
 // Calculo do total
 // Navegação no clique do TouchableHighlight
 
+interface Product {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
+  quantity: number;
+}
+
 const FloatingCart: React.FC = () => {
   const { products } = useCart();
 
@@ -25,14 +33,23 @@ const FloatingCart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products
+      ? products.reduce((acc: number, product: Product) => {
+          return acc + product.quantity * product.price;
+        }, 0)
+      : 0;
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const total = products
+      ? products.reduce((acc: number, product: Product) => {
+          return acc + product.quantity;
+        }, 0)
+      : 0;
+    return total;
   }, [products]);
 
   return (
